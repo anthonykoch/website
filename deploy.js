5b940c9441524be0113aa84be7ca1fe9348ea1b1
anthonykoch <anthonykoch@users.noreply.github.com>
2018-07-29 05:17:29 -0700

    Fix deploy script

--- deploy.js
+++ deploy.js
@@ -3,6 +3,7 @@ const ghpages = require('gh-pages');
 ghpages.publish('dist', {
   dotfiles: true,
   branch: 'master',
+  repo: 'git@github.com:anthonykoch/anthonykoch.github.io.git',
 }, (err) => {
   if (err) {
     console.log(err);
